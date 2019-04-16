8e919e438ccee2b5a2bd99e49985f9d6bdabe050
Rodrigo Alfonso <rodri042@gmail.com>
2019-04-16 03:10:58 -0300

    Adding quick routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
-	state = {};
+	state = { isConnected: false };
 
 	render() {
-		return <div>// TODO</div>;
+		switch (window.location.hash) {
+			case "#/player1":
+				return <div>player1</div>;
+			case "#/player2":
+				return <div>player2</div>;
+			default:
+				return <div>You need to use either /#/player1 or /#/player2</div>;
+		}
+
+		return (
+			<div>
+				<button onClick={this.connect}>Connect</button>
+				<button onClick={this.disconnect} disabled={!this.state.isConnected}>
+					Disconnect
+				</button>
+			</div>
+		);
+	}
+
+	componentWillMount() {
+		this._listener = window.addEventListener("hashchange", (e) => {
+			this.forceUpdate();
+		});
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener("hashchange", this._listener);
 	}
 }
