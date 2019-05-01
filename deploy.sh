c61aaaef565d41fdc271a3d0a4c1d6bad4be7f6d
Rodrigo Alfonso <rodri042@gmail.com>
2019-05-01 19:07:40 -0300

    Adding deploy script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function try {
+	"$@"
+	local status=$?
+	if [ $status -ne 0 ]; then
+		echo "!!!Error!!! with $1" >&2
+		exit $status
+	fi
+	return $status
+}
+
+rm -rf build/
+try npm run build
+cd build/
+git init
+git add -A .
+git commit -m "Deploy"
+git remote add origin https://github.com/rodri042/quickp2p
+git push -f origin master:gh-pages
